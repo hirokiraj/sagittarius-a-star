c1b4c13182898052d13b8166ae653062ea81f81d
hirokiraj <jatczak.j@gmail.com>
2024-04-25 23:39:23 +0200

    Add code diagnostics full message
    As I stated working with more LSPs, I needed to add a shortcut for
    displaying a full lsp diagnostic message in a floating window. Inline
    diagnostics are ok for fast checking things, but sometimes I need to
    actually read the whole message and I'd prefer to show it in a separate,
    easy to close floating window on top of loc.

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -511,6 +511,7 @@ require('lazy').setup({
           --  For example, in C this would take you to the header
           map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
 
+          map('<leader>cd', vim.diagnostic.open_float, '[C]ode [D]iagnostics in float window')
           -- The following two autocommands are used to highlight references of the
           -- word under your cursor when your cursor rests there for a little while.
           --    See `:help CursorHold` for information about when this is executed
