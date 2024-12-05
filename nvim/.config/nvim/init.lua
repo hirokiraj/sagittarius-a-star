1c7f698db3103e6fc0b92669aefa5f806d7bca57
hirokiraj <jatczak.j@gmail.com>
2024-12-05 18:25:10 +0100

    Neovim - show gitignored files in tree
    By default nvim-tree plugin hides files if these are gitignored, but I
    want to be able to navigate to those in tree pane so I'm overwriting
    this default setting.

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -898,7 +898,11 @@ require("lazy").setup({
 			"nvim-tree/nvim-web-devicons",
 		},
 		config = function()
-			require("nvim-tree").setup({})
+			require("nvim-tree").setup({
+				git = {
+					ignore = false,
+				},
+			})
 		end,
 	},
 }, {
